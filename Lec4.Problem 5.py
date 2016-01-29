1006b7252738722ddc272abad3f0d655d9c30662
mohcoder <moh.zaher.coder@gmail.com>
2016-01-29 19:28:26 +0300

    Lecture 4, Problem 5

---
+++ Lec4.Problem 5.py
@@ -0,0 +1,10 @@
+def clip(lo, x, hi):
+    '''
+    Takes in three numbers and returns a value based on the value of x.
+    Returns:
+     - lo, when x < lo
+     - hi, when x > hi
+     - x, otherwise
+    '''
+    # Your code here
+    return min(max(x, lo), hi)
