ba2524a68b798ab0d3e840816c1f6212419740c4
mohcoder <moh.zaher.coder@gmail.com>
2016-01-29 19:35:07 +0300

    Problem Set 1, Counting Bobs

---
+++ ProblemSet1_CountingBobs.py
@@ -0,0 +1,14 @@
+search = 'bob'
+
+def countVowels(s):
+    count = 0
+    for i in range(len(s)):
+        if s[i:i+3] == search:
+            count += 1
+    countPrint = ("Number of times %s occurs is: %s" %  (search, count))
+    return countPrint
+
+def printCount():
+    print(countVowels(s))
+
+printCount()
