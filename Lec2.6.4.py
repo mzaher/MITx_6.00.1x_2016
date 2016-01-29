446c8548de09eebc9dc212446d915d3415431d86
mohcoder <moh.zaher.coder@gmail.com>
2016-01-29 19:44:53 +0300

    Lec 2.6, slide 4

--- Lec2.6.4.py
+++ Lec2.6.4.py
@@ -2,10 +2,10 @@
 
 x = 6
 
-if x%2 == 0:
-    if x%3 == 0:
+if x % 2 == 0:
+    if x % 3 == 0:
         print('Divisible by 2 and 3')
     else:
         print('Divisible by 2 and not by 3')
-elif x%3 == 0:
+elif x % 3 == 0:
     print('Divisible by 3 and not by 2')
